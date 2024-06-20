c23fc2777edc03af9b538fa6f14f2588cbc95469
kiruha17 <wangterry36b@gmail.com>
2024-06-20 12:17:12 -0400

    Add bootstrap and nav bar

--- index.js
+++ index.js
@@ -8,5 +8,9 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
-  });
+  res.render("index.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
