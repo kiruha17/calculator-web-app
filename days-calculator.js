226b36815717da3b5f5e93d681c7037b3dad17a4
kiruha17 <wangterry36b@gmail.com>
2024-06-25 20:53:18 -0400

    Style days calculator

--- days-calculator.js
+++ days-calculator.js
@@ -7,14 +7,14 @@ $(".select-date").on('change', '#date', function() {
     let isTooClose = false;
     let inPast = false;
     let differenceInDays = Math.round(differenceInTime / (1000 * 3600 * 24));
-    if (differenceInDays < 1 && differenceInDays > -1) {isTooClose = true;}
+    if (differenceInDays < 2 && differenceInDays > -3) {isTooClose = true;}
     if (differenceInDays < -1) {inPast = true;}
 
     let message = "";
     if (isTooClose) {
         message = "The dates are too close and rounding errors are likely, please choose a further date.";
     } else if (inPast) {
-        message = `The date you've entered is ${differenceInDays * -1} days ago.`;
+        message = `The date you've entered is ${(differenceInDays * -1) - 1} days ago.`;
     } else {
         message = `The date you've entered is ${differenceInDays} days from now.`
     }
